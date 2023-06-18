bcc832aa8103cf114bb87aafdeced9f7eda25ae0
Hfear <hannahbfearns@gmail.com>
2023-06-18 14:24:00 -0400

    sum of range, range works

---
+++ SumofaRange.js
@@ -0,0 +1,27 @@
+
+function range(start, end)
+{
+    let arr = [];
+
+    for(let i = start; i <= end; i++)
+    {
+        arr.push(i);
+    }
+    return arr; 
+}
+
+function sum(arr)
+{
+    let sum = 0;
+    for(let i = 0; i< arr.length; i++)
+    {
+        //console.log(i);
+        sum = sum + arr[i];
+    }
+    return sum;
+}
+
+
+let arr = [[1,2,3,4,5]];
+//console.log(range(5,8));
+console.log(sum(arr));
