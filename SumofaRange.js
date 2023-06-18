9adfca84aa72701113b9ff7f775a1a2282cc545e
Hfear <hannahbfearns@gmail.com>
2023-06-18 16:25:32 -0400

    fixed sum of range

--- SumofaRange.js
+++ SumofaRange.js
@@ -13,15 +13,17 @@ function range(start, end)
 function sum(arr)
 {
     let sum = 0;
-    for(let i = 0; i< arr.length; i++)
+    for(let i = 0; i < arr.length; i++)
     {
-        //console.log(i);
+        //console.log(arr[i]);
+        //console.log("hi");
         sum += arr[i];
     }
+
     return sum;
 }
 
 
-let arr = [[1,2,3,4,5]];
+let arr = [1,2,3,4,5];
 //console.log(range(5,8));
 console.log(sum(arr));
